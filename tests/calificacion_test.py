9dcafe09b60d12db5b62edefaa7f8091811adf8b
Víctor Vázquez <victorvazrod@correo.ugr.es>
2019-10-31 14:26:30 +0100

    Elimina clase de tests
    Avanza la tarea #6

--- tests/calificacion_test.py
+++ tests/calificacion_test.py
@@ -1,29 +1,27 @@
 import pytest
 
-from src.calificacion.definition import Calificacion
-from src.calificacion.exceptions import *
+from calificacion.definition import Calificacion
+from calificacion.exceptions import *
 
-class TestCalificacion:
-    
-    def nota_correcta(self):
-        _ = Calificacion("Alumno", 6, "Comentario de prueba")
+def test_nota_correcta():
+    _ = Calificacion("Alumno", 6, "Comentario de prueba")
 
-    def nota_negativa(self):
-        with pytest.raises(NotaException):
-            _ = Calificacion("Alumno", -3, "Comentario de prueba")
+def test_nota_negativa():
+    with pytest.raises(NotaException):
+        _ = Calificacion("Alumno", 11, "Comentario de prueba")
 
-    def alumno_none(self):
-        with pytest.raises(AlumnoNoneException):
-            _ = Calificacion(None, 6, "Comentario de prueba")
+def test_alumno_none():
+    with pytest.raises(AlumnoNoneException):
+        _ = Calificacion(None, 6, "Comentario de prueba")
 
-    def alumno_empty(self):
-        with pytest.raises(AlumnoEmptyException):
-            _ = Calificacion("", 6, "Comentario de prueba")
+def test_alumno_empty():
+    with pytest.raises(AlumnoEmptyException):
+        _ = Calificacion("", 6, "Comentario de prueba")
 
-    def comentario_none(self):
-        with pytest.raises(ComentarioNoneException):
-            _ = Calificacion("Alumno", 6, None)
+def test_comentario_none():
+    with pytest.raises(ComentarioNoneException):
+        _ = Calificacion("Alumno", 6, None)
 
-    def comentario_empty(self):
-        with pytest.raises(ComentarioEmptyException):
-            _ = Calificacion("Alumno", 6, "")
+def test_comentario_empty():
+    with pytest.raises(ComentarioEmptyException):
+        _ = Calificacion("Alumno", 6, "")
